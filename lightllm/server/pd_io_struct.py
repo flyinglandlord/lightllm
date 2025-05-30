bb2182f56e0f4ce9637fe809ef1f471947c0d7a9
Weichao Luo <luoweichao@sensetime.com>
2025-05-30 10:18:23 +0800

    fix lint.

--- lightllm/server/pd_io_struct.py
+++ lightllm/server/pd_io_struct.py
@@ -133,6 +133,7 @@ class RemotePrefillServerInfo:
     prefill_server_ip: str
     prefill_server_port: int
 
+
 @dataclass
 class DistInfo:
     world_size: int
@@ -141,6 +142,7 @@ class DistInfo:
     dp_world_size: int
     dp_size_in_node: int
     node_world_size: int
+
 
 @dataclass
 class PDTransLeaveInfo:

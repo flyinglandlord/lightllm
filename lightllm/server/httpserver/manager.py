2d7e9b5e26da52e438f7ca3ef863f90ec2f4b0b0
Weichao Luo <luoweichao@sensetime.com>
2025-06-24 17:19:14 +0800

    fix chunked transfer & bugs.

--- lightllm/server/httpserver/manager.py
+++ lightllm/server/httpserver/manager.py
@@ -454,7 +454,7 @@ class HttpServerManager:
         # 多节点纯tp 运行模式下，master 节点需要将请求转发给slave节点.
         if self.is_multinode_tp_master:
             for sender in self.multinode_req_manager:
-                sender.send_pyobj(
+                await sender.send_pyobj(
                     (prompt, sampling_params, original_multimodal_params),
                     protocol=pickle.HIGHEST_PROTOCOL,
                 )
@@ -469,12 +469,14 @@ class HttpServerManager:
 
         if self.pd_mode.is_P():
             if self.enable_multimodal:
-                self.send_to_visual.send_pyobj(
+                await self.send_to_visual.send_pyobj(
                     group_req_objs.to_group_req_index(),
                     protocol=pickle.HIGHEST_PROTOCOL,
                 )
             else:
-                self.send_to_router.send_pyobj(
+
+                # P 模式下，直接将请求发送到路由器
+                await self.send_to_router.send_pyobj(
                     group_req_objs.to_group_req_index(),
                     protocol=pickle.HIGHEST_PROTOCOL,
                 )
@@ -482,7 +484,7 @@ class HttpServerManager:
 
         if self.pd_mode.is_D():
             # 在 D 模式下，不需要传输真的多模态参数，因为其已经被 P 处理好了, 传输一个空的即可
-            self.send_to_router.send_pyobj(
+            await self.send_to_router.send_pyobj(
                 group_req_objs.to_group_req_index(),
                 protocol=pickle.HIGHEST_PROTOCOL,
             )
@@ -490,12 +492,12 @@ class HttpServerManager:
 
         if self.pd_mode.is_normal():
             if self.enable_multimodal:
-                self.send_to_visual.send_pyobj(
+                await self.send_to_visual.send_pyobj(
                     group_req_objs.to_group_req_index(),
                     protocol=pickle.HIGHEST_PROTOCOL,
                 )
             else:
-                self.send_to_router.send_pyobj(
+                await self.send_to_router.send_pyobj(
                     group_req_objs.to_group_req_index(),
                     protocol=pickle.HIGHEST_PROTOCOL,
                 )

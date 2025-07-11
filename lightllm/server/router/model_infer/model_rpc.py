b0e51faa1ecdab5c625cd4e9f3ddae3da2576dd1
Weichao Luo <luoweichao@sensetime.com>
2025-07-11 18:56:50 +0800

    fix lint.

--- lightllm/server/router/model_infer/model_rpc.py
+++ lightllm/server/router/model_infer/model_rpc.py
@@ -124,8 +124,8 @@ class ModelRpcServer:
         assert not (is_outlines_constraint_mode and is_xgrammar_constraint_mode), "only one constraint mode can be true"
         is_prefill_node = self.args.run_mode == "prefill"
         is_decode_node = self.args.run_mode == "decode"
-        is_nixl_prefill_node = self.args.run_mode == 'nixl_prefill'
-        is_nixl_decode_node = self.args.run_mode == 'nixl_decode'
+        is_nixl_prefill_node = self.args.run_mode == "nixl_prefill"
+        is_nixl_decode_node = self.args.run_mode == "nixl_decode"
 
         if is_prefill_node:
             if self.args.dp > 1:
@@ -133,6 +133,10 @@ class ModelRpcServer:
             else:
                 self.backend = ChunckedPrefillForPrefillNode(self.info_queue, self.mem_queue)
         elif is_nixl_prefill_node:
+<<<<<<< HEAD
+=======
+            assert not enable_mtp, "nixl pd does not support mtp now."
+>>>>>>> a9995b3... fix lint.
             if self.args.dp > 1:
                 self.backend = PDNIXLDPBackendForPrefillNode(self.info_queue, self.result_queue, self.mem_queue)
             else:
@@ -145,6 +149,10 @@ class ModelRpcServer:
                 self.backend = DecodeNode(self.info_queue, self.mem_queue)
 
         elif is_nixl_decode_node:
+<<<<<<< HEAD
+=======
+            assert not enable_mtp, "nixl pd does not support mtp now."
+>>>>>>> a9995b3... fix lint.
             if self.args.dp > 1:
                 self.backend = PDNIXLDPBackendForDecodeNode(self.info_queue, self.result_queue, self.mem_queue)
             else:

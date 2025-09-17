96234fafbe54cd8b41e831a7adf200e2ac92c56b
wangzaijun <wangzaijun@sensetime.com>
2025-09-17 07:15:09 +0000

    fix

--- lightllm/server/router/req_queue/__init__.py
+++ lightllm/server/router/req_queue/__init__.py
@@ -1,6 +1,7 @@
 from .chunked_prefill.impl_for_pd_decode import QueueForPDDecode
 from .chunked_prefill.impl import ChunkedPrefillQueue
 from .chunked_prefill.beam_impl import ChunkedBeamContinuesBatchQueue
+from .chunked_prefill.impl_for_nixl_pd import NIXLPDQueue
 from .dp_base_queue import DpQueue
 
 
@@ -15,8 +16,10 @@ def _get_req_queue_class(args, router, dp_size_in_node: int):
         return ChunkedPrefillQueue
     if args.run_mode in ["decode"]:
         return QueueForPDDecode
-    if args.run_mode in ["prefill", "nixl_prefill", "nixl_decode"]:
+    if args.run_mode in ["prefill"]:
         return ChunkedPrefillQueue
+    if args.run_mode in ["nixl_prefill", "nixl_decode"]:
+        return NIXLPDQueue
 
     if args.disable_chunked_prefill:
         # 虽然也使用chuncked prefill queue 但是由于 args.chunked_prefill_size = args.max_req_total_len

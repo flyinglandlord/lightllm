f7ed41639f9473d579f8eea4428e49f63159c45e
Junyi Chen <chenjunyi@sensetime.com>
2026-04-13 17:03:21 +0800

    push

--- test/benchmark/static_inference/profile_demo.py
+++ test/benchmark/static_inference/profile_demo.py
@@ -3,8 +3,6 @@ import numpy as np
 from torch.profiler import profile, record_function, ProfilerActivity
 
 draft_probs_tensor = torch.rand(10, 4).cuda()  # Simulating draft probabilities for 10 steps and batch size of 4
-rand_vals = torch.rand_like(draft_probs_tensor)
-accepted_mask = draft_probs_tensor > rand_vals
 torch.cuda.synchronize()
 with profile(
     activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
@@ -12,6 +10,8 @@ with profile(
     profile_memory=False,
     on_trace_ready=torch.profiler.tensorboard_trace_handler("./log/"),
 ) as prof:
+    rand_vals = torch.rand_like(draft_probs_tensor)
+    accepted_mask = draft_probs_tensor > rand_vals
     valid_steps = torch.cumprod(accepted_mask.to(torch.int32), dim=0)
     dynamic_mtp_sizes = valid_steps.sum(dim=0)
     pass
